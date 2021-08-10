9acaaae8017cdad9c3bbc62643a62cb2fbd8f2b6
Kaif Jeelani <47492167+kjeelani@users.noreply.github.com>
2021-08-09 22:36:15 -0700

    Uploaded final model, code, and loss/accuracy graphs

--- process_data.py
+++ process_data.py
@@ -7,6 +7,7 @@ from glob import glob
 from PIL import Image
 #ML Imports
 import keras
+from keras import callbacks
 import numpy as np
 from keras.utils.np_utils import to_categorical
 from keras.models import Sequential
@@ -17,7 +18,7 @@ from scipy import stats
 from sklearn.preprocessing import LabelEncoder
 
 np.random.seed(60)
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, plot_confusion_matrix
 from sklearn.utils import resample
 
 '''
@@ -55,12 +56,15 @@ metadata['dx'].value_counts().plot(kind='bar', ax=axis)
 axis.set_ylabel('Count')
 axis.set_xlabel('Labels')
 axis.set_title('Cancer Type')
+plt.xticks(rotation=0)
 plt.show()
 '''
 
+
+
 #Balance Data
 unbalanced_dfs = [metadata[metadata['id'] == i] for i in range(7)]
-N = 775
+N = 700
 balanced_df = pd.concat([resample(df, replace=True, n_samples=N, random_state=50) for df in unbalanced_dfs])
 
 #Read Images
@@ -73,40 +77,50 @@ X = np.asarray(balanced_df['image'].tolist())
 X = X/255
 Y = balanced_df['id']
 Y = to_categorical(Y, num_classes=7)
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=.3, random_state=60)
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=.25, random_state=60)
+
+#Augment Data
+aug = ImageDataGenerator(
+    width_shift_range=2,
+    height_shift_range=2,
+    rotation_range=25,
+    zoom_range=.15,
+)
 
 #Create model
 model = Sequential([
-    Conv2D(256, (3,3), activation="relu", input_shape=(PIXEL_SIZE, PIXEL_SIZE, 3)),
-    MaxPool2D(pool_size=(2,2)),
-    Dropout(0.3),
+    Conv2D(64, (3,3), activation="relu", input_shape=(PIXEL_SIZE, PIXEL_SIZE, 3)),
+    MaxPool2D(pool_size=(2,2), padding="same"),
+    Dropout(0.25),
 
     Conv2D(128, (3,3), activation="relu"),
-    MaxPool2D(pool_size=(2,2)),
-    Dropout(0.3),
+    MaxPool2D(pool_size=(2,2), padding="same"),
+    Dropout(0.25),
 
-    Conv2D(64, (3,3), activation="relu"),
-    MaxPool2D(pool_size=(2,2)),
-    Dropout(0.3),
+    Conv2D(256, (3,3), activation="relu"),
+    MaxPool2D(pool_size=(2,2), padding="same"),
+    Dropout(0.25),
     Flatten(),
 
-    Dense(32),
-    Dense(16),
+    Dense(512, activation="relu"),
     Dense(7, activation="softmax")
 ])
 
 model.summary()
-model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 #Train model
-batch_size = 16
-epochs = 50
+batch_size = 32
+epochs = 100
 
+earlystopping = callbacks.EarlyStopping(monitor ="val_loss", 
+                                        mode ="min", patience = 6, 
+                                        restore_best_weights = True)
 hist = model.fit(
-    x_train, y_train,
+    x = aug.flow(x_train,y_train,batch_size=batch_size),
     epochs = epochs,
-    batch_size = batch_size,
     validation_data = (x_test,y_test),
+    callbacks=[earlystopping],
     verbose=2
 )
 
@@ -114,17 +128,24 @@ score = model.evaluate(x_test, y_test)
 print('Test accuracy', score[1])
 
 
-#plot training epochs
-'''
-loss = hist.history['acc']
-epochs = range(1, len(loss) + 1)
-plt.plot(epochs, loss, 'y', label='Training Accuracy')
-plt.title('Training Accuracy')
-plt.xlabel('Epochs')
+#plot training and validation for accuracy and loss
+plt.plot(hist.history['accuracy'])
+plt.plot(hist.history['val_accuracy'])
+plt.title('Model Accuracy')
 plt.ylabel('Accuracy')
-plt.legend()
+plt.xlabel('Epoch #')
+plt.legend(['train', 'test'], loc='upper left')
+plt.savefig("graphs/accuracyTest.png")
 plt.show()
-'''
+
+plt.plot(hist.history['loss'])
+plt.plot(hist.history['val_loss'])
+plt.title('Model Loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch #')
+plt.legend(['train', 'validation'], loc='upper left')
+plt.savefig("graphs/lossTest.png")
+plt.show()
 
 
 #Saving the model for future reference
@@ -133,13 +154,3 @@ with open("models/model.json", "w") as jf:
     jf.write(model_json)
 model.save_weights("models/model.h5")
 print("Model Saved")
-
-
-#Usage of model
-'''
-jf = open('models/model.json', 'r')
-model = jf.read()
-jf.close()
-model = model_from_json(model)
-model.load_weights("model.h5")
-'''
